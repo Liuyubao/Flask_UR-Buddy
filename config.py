b3c982cb4a7325607680502fbf8acdec7c988e10
Yubao Liu <louis.liu@bytedance.com>
2021-03-16 11:46:23 -0400

    init

---
+++ config.py
@@ -0,0 +1,14 @@
+
+DEBUG = False
+
+# connect mysql
+DIALECT = 'mysql'
+DRIVER = 'pymysql'
+USERNAME = 'root'
+PASSWORD = 'Lyb520520'
+HOST = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'URBuddy_db'
+
+SQLALCHEMY_DATABASE_URI = "{}+{}://{}:{}@{}:{}/{}?charset=utf8".format(DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
